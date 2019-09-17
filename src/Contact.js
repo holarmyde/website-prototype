2374a6d9544cfb81c671873d213fce98c7db0a56
Oluwafemi Aremu <ooa255@nyu.edu>
2019-09-17 16:28:02 -0400

    grace

---
+++ src/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+      </p>
+
+  </div>
+)
